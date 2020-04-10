35a2fedf4e5ae669fdeb53c4c49f8ce3301158cc
work <26752232+githubsands@users.noreply.github.com>
2020-04-10 09:26:55 -0600

    httpclient beginnings

--- http/httpclient.go
+++ http/httpclient.go
@@ -1,20 +1,13 @@
 package main
 
 import (
-	"bytes"
-	"crypto/tls"
-	"crypto/x509"
-	"encoding/json"
 	"fmt"
 	"io/ioutil"
-	"net"
 	"net/http"
-
-	"github.com/githubsands/twittercli/twitterjson"
 )
 
 type config struct {
-	TLSSkipVerify bool 
+	TLSSkipVerify bool
 }
 
 type Client interface {
@@ -22,102 +15,121 @@ type Client interface {
 }
 
 type TwitterClient struct {
-	// other attributes here 
-	httpClient *http.Client 
+	marshalResponse bool
+	// other attributes here
+	httpClient *http.Client
 }
 
 // NewHTTPClient returns a new HTTP client that is configured according to the
 // proxy and TLS settings in the associated connection configuration.
-func NewTwitterClient(cfg *config) (*TwitterClient, error) {
+func NewTwitterClient(mr bool) (*TwitterClient, error) {
+
 	// Configure proxy if needed.
-	var dial func(network, addr string) (net.Conn, error)
+	/*
+		var dial func(network, addr string) (net.Conn, error)
 
-	dial = func(network, addr string) (net.Conn, error) {
-		c, err := proxy.Dial(network, addr)
-		if err != nil {
-			return nil, err
-		}
-		return c, nil
-	}
-
-	// Configure TLS if needed.
-	var tlsConfig *tls.Config
-	if !cfg.NoTLS {
-		tlsConfig = &tls.Config{
-			InsecureSkipVerify: cfg.TLSSkipVerify,
-		}
-		if !cfg.TLSSkipVerify {
-			pem, err := ioutil.ReadFile(cfg.RPCCert)
+		dial = func(network, addr string) (net.Conn, error) {
+			c, err := proxy.Dial(network, addr)
 			if err != nil {
-				return nil, err } pool := x509.NewCertPool()
-			if ok := pool.AppendCertsFromPEM(pem); !ok {
-				return nil, fmt.Errorf("invalid certificate file: %v",
-					cfg.RPCCert)
+				return nil, err
 			}
-			tlsConfig.RootCAs = pool
+			return c, nil
 		}
-	}
+
+		// Configure TLS if needed.
+		var tlsConfig *tls.Config
+		if !cfg.NoTLS {
+			tlsConfig = &tls.Config{
+				InsecureSkipVerify: cfg.TLSSkipVerify,
+			}
+			if !cfg.TLSSkipVerify {
+				pem, err := ioutil.ReadFile(cfg.RPCCert)
+				if err != nil {
+					return nil, err } pool := x509.NewCertPool()
+				if ok := pool.AppendCertsFromPEM(pem); !ok {
+					return nil, fmt.Errorf("invalid certificate file: %v",
+						cfg.RPCCert)
+				}
+				tlsConfig.RootCAs = pool
+			}
+		}
+	*/
 
 	// Create and return the new HTTP client potentially configured with a
 	// proxy and TLS.
-	client := http.Client{
-		Transport: &http.Transport{
-			Dial:            dial,
-			TLSClientConfig: tlsConfig,
-		},
+	client := &http.Client{
+		/*
+			Transport: &http.Transport{
+				Dial:            dial,
+				TLSClientConfig: tlsConfig,
+			},
+		*/
 	}
 
 	return &TwitterClient{
-		httpClient: client,
+		marshalResponse: mr,
+		httpClient:      client,
 	}, nil
 }
 
-func (t *TwitterClient) GetHTTPRequestBody([]byte, error) {
-	httpResponse, err := httpClient.Do(req)
+func (t *TwitterClient) NewTwitterRequest() (*http.Request, error) {
+	req, err := http.NewRequest("POST", "www.twitter.com", nil)
 	if err != nil {
 		return nil, err
 	}
 
-	if httpResponse.StatusCode < 200 || httpResponse.StatusCode >= 300 {
+	return req, nil
+}
+
+// GetHTTPResponseBody takes a request, sends it, and returns the response
+func (t *TwitterClient) GetHTTPResponse(req *http.Request) (*http.Response, error) {
+	res, err := t.httpClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	// Read the raw bytes and close the response.
+	respBytes, err := ioutil.ReadAll(res.Body)
+	res.Body.Close()
+	if err != nil {
+		err = fmt.Errorf("error reading reply: %v", err)
+		return nil, err
+	}
+
+	if res.StatusCode < 200 || res.StatusCode >= 300 {
 		if len(respBytes) == 0 {
-			return nil, fmt.Errorf("%d %s", httpResponse.StatusCode,
-				http.StatusText(httpResponse.StatusCode))
+			return nil, fmt.Errorf("%d %s", res.StatusCode,
+				http.StatusText(res.StatusCode))
 		}
 		return nil, fmt.Errorf("%s", respBytes)
 	}
 
+	return res, nil
+}
+
+func (t *TwitterClient) GetHTTPResponseBodyBytes(req *http.Request) ([]byte, error) {
+	res, err := t.httpClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
 	// Read the raw bytes and close the response.
-	respBytes, err := ioutil.ReadAll(httpResponse.Body)
-	httpResponse.Body.Close()
+	respBytes, err := ioutil.ReadAll(res.Body)
+	res.Body.Close()
 	if err != nil {
 		err = fmt.Errorf("error reading reply: %v", err)
 		return nil, err
+	}
+
+	if res.StatusCode < 200 || res.StatusCode >= 300 {
+		if len(respBytes) == 0 {
+			return nil, fmt.Errorf("%d %s", res.StatusCode,
+				http.StatusText(res.StatusCode))
+		}
+		return nil, fmt.Errorf("%s", respBytes)
 	}
 
 	return respBytes, nil
 }
 
-func (t *TwitterClient) GetHTTPRequest(req *http.Request) (http.Request, error) {
-	httpResponse, err := httpClient.Do(req)
-	if err != nil {
-		return nil, err
-	}
-
-	if httpResponse.StatusCode < 200 || httpResponse.StatusCode >= 300 {
-		if len(respBytes) == 0 {
-			return nil, fmt.Errorf("%d %s", httpResponse.StatusCode,
-				http.StatusText(httpResponse.StatusCode))
-		}
-		return nil, fmt.Errorf("%s", respBytes)
-	}
-
-	// Read the raw bytes and close the response.
-	respBytes, err := ioutil.ReadAll(httpResponse.Body)
-	httpResponse.Body.Close()
-	if err != nil {
-		err = fmt.Errorf("error reading reply: %v", err)
-		return nil, err
-	}
-
-	return httpResponse, nil
-}
+func (t *TwitterClient) UnmarshalResponse() bool { return t.marshalResponse }
